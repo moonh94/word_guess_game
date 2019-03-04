9d4aa28810482326c3379504ffe328b1fb97da43
Han Moon <hanmoon@Hans-MacBook-Pro.local>
2019-03-03 22:54:26 -0700

    js changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,40 +1,42 @@
 //variables
-var question = ["cars", "mulan", "toy story", "cinderella", "frozen"];
+var word = ["cars", "mulan", "toy story", "cinderella", "frozen"];
 var letterGuessed = [];
+var question = ["----"];
 var alphabet = ["a", "b", "c", "d", "e", "f", "g",
     "h", "i", "j", "k", "l", "m", "n", "o", "p", "q",
     "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var actualWord = "";
-
-
-for (var i = 0; i < question.length; i++) 
-
-    //random word/question
-    actualWord = question[Math.floor(Math.random() * question.length)];
-        
-   
+var cars = ["c", "a", "r", "s",];
+var car = cars[i];
+var mulan = ["m", "u", "l", "a", "n"];
 
 
 
 
-
-    var num = 0;
-    var score = 0;
-    var questionIndex = 0;
+//random word/question
+actualWord = question[Math.floor(Math.random() * question.length)];
 
 
-    //function
+
+var num = 0;
+var score = 0;
+console.log(score);
+var questionIndex = 0;
+
+
+//function
+//random question
+for (var i = 0; i < question.length; i++) {
     //show next question
     function renderQuestion() {
         if (questionIndex <= (question.length - 1)) {
-            document.querySelector("#question").innerHTML = question[questionIndex];
+            document.querySelector("#unknownWord").innerHTML = question[questionIndex];
 
         } else {
             //end game if we run out of questions
-            document.querySelector("#question").innerHTML = "Game Over";
+            document.querySelector("#unknownWord").innerHTML = "Game Over";
         }
     }
-    
+
     //update score
     function updateScore() {
         document.querySelector("#score").innerHTML = "Score: " + score;
@@ -46,26 +48,46 @@ for (var i = 0; i < question.length; i++)
     updateScore();
     renderQuestion();
 
-    document.onkeyup = function (event) {
+
+    document.onkeyup = function () {
 
         var userGuess = event.key;
         console.log(userGuess);
 
-        //randomly chooses next question
 
-        userGuess = document.querySelector("#question").innerHTML = question[Math.floor(Math.random() * question.length)];
 
         if (questionIndex === question.length) {
-
             return;
         }
-        if (userGuess === ("c" || "a" || "r" || "s")) {
-            alphabet.push("<question>");
+
+        if (event.key === "c") {
+            console.log(event.key);
         } else {
-            alphabet.push("#letterGuessed");
-        } 
+            document.querySelector("#letters-guessed")
         }
-    
+
+
+    }
+}
+
+
+
+
+        // if (userGuess === ("c")) {
+        //     alphabet.push("");
+        // } else {
+        //     alphabet.push("#letterGuessed");
+        // } 
+        // if (userGuess="c") {
+        //     car.push(car[1]).textContext("#question");
+
+
+        // if (animal.toLowerCase().charAt(0) === "c") {
+        //     car.push(car[1]);
+        // }
+
+
+
     // if (userGuess === "c") {
     //     document.querySelector("#question").innerHTML = "c----";
     // } else {
